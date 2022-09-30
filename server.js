b59b5944c65dfff9d531a71d22007012311032dc
JhovanyMurgia <98408956+JhovanyMurgia@users.noreply.github.com>
2022-09-30 10:05:15 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const http = require('http');
 
-const hostname = '127.0.0.1';
 const porta =  process.env.porta || 3000;
 
 const server = http.createServer((req, res) => {
@@ -10,5 +9,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(porta, hostname, () => {
-    console.log(`Server running at http://${hostname}:${porta}/`);
+    console.log(`Server rodando em http://localhost:${porta}`);
 });
