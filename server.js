4ae931f006b36ec09558e7370104d781799891eb
JhovanyMurgia <98408956+JhovanyMurgia@users.noreply.github.com>
2022-09-30 09:26:24 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const hostname = '127.0.0.1';
-const porta =  process.env.PORT || 3000;
+const porta =  process.env.porta || 3000;
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
