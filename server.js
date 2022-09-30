89e3b19036d313282546772193a6c1061716791c
JhovanyMurgia <98408956+JhovanyMurgia@users.noreply.github.com>
2022-09-30 09:03:01 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const porta =  3000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Acessando o servidor');
+});
+
+server.listen(porta, hostname, () => {
+    console.log(`Server running at http://${hostname}:${porta}/`);
+});
